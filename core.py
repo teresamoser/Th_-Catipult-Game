87b2243b1166bb3f325192f9d0d7edbf7198050d
jsappbyui1 <sap19001@byui.edu>
2021-10-12 23:30:43 -0600

    Outline

---
+++ core.py
@@ -0,0 +1,18 @@
+import arcade
+import math
+
+
+class physics:
+    pass
+
+class GUI:
+    #The User interface.  This class contains all elements visible to the user.
+    
+    class room:
+        #the space in wich the game takes place.  This class defines its area, properties, and other details.
+        pass
+
+    class camera:
+        #controlls what section of room is visible at any given time
+        pass
+
