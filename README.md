9dca5f604bbe66fe7c72295b47c64edfab3b366a
jsappbyui1 <sap19001@byui.edu>
2021-10-13 00:40:47 -0600

    settup 10/12

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Th_-Catipult-Game
+
+## Code Formating
+To keep things orginized, here are the formating conventions for the code:
+- Variables:  all lowercase using underscores to seperate words.  (my_varible)
+- Classes: Prefrebly one word, capitilized.  (Engine)
+
+## Filestructure
+The game is composed primarily of three main files, with all other assets contained within the appropriate "Assets" sub folder.  The Core.py file conains the main game, while the game_settings.csv file works as long term storage for the game.  The game_save.csv file keeps track to run sespific elements of the game, tracking user informaion for future use.
+
+## core.py Structure
+The core.py file is structured into two main components, the Engine and the Interface.
+- Interface:  The interface is responsible for managing all graphical elements, allowing the user to see into the game world.  This includes the game window (the section of screen that is the game), The room (the 'location' that the game is taking place in), and the camera (the secion of the room that is displayed at any given time)
+- Engine: the Engine is responsible for handling all the elements that the user does not directly see.  Game objects, the Physics engine, and save/load systems all fall into this catagory.  
+
+
+
--- core.py
+++ core.py
@@ -1,32 +1,82 @@
+#%%
+
 import arcade
 import math
+import csv
 
 
-class engine:
+class Engine:
     # What runs the game.  This is for everything not visible to the player (mechanicly speeking)
 
-    class physics:
+    def start(self):
+        #performs the startup actions of the game, such as showing a logo and importing all save data
+        self.load_settings()
+        self.Interface.Window(game_settings.get("SCREEN_WIDTH"),game_settings.get("SCREEN_HEIGHT"),game_settings.get("SCREEN_TITLE"))
+        arcade.run()
+
+    def load_settings(self):
+        global game_settings
+        reader = csv.reader(open('game_settings.csv','r'))
+        game_settings = {}
+        for row in reader:
+            k, v = row
+            try:
+                v = int(v)
+            except:
+                pass
+            game_settings[k] = v
+        print(game_settings)
+
+    class Physics:
         #the physics that runs the world and applys to all objects 
 
         gravity = 9.8
 
-    class object():
+    class Object():
         #each item in the world that is not static is an object.  
         pass
 
 
-class GUI:
-    #The User interface.  This class contains all elements visible to the user.
+    class Interface:
+        #The User interface.  This class contains all elements visible to the user.
 
-    class window:
-        # the device that is used to view the game.  It is the box that appears on screen
-        pass
+        class Window(arcade.Window):
+            # the device that is used to view the game.  It is the box that appears on screen
+            
+            def __init__(self, width, height, title):
+                super().__init__(width, height, title)
 
-    class room:
-        #the space in wich the game takes place.  This class defines its area, properties, and other details.  Each room is a single level.
-        pass
+                arcade.set_background_color(arcade.color.AMAZON)
 
-    class camera:
-        #controlls what section of room is visible at any given time
-        pass
+                # If you have sprite lists, you should create them here,
+                # and set them to None
 
+            def on_draw(self):
+                """
+                Render the screen.
+                """
+
+                # This command should happen before we start drawing. It will clear
+                # the screen to the background color, and erase what we drew last frame.
+                arcade.start_render()
+
+                # Call draw() on all your sprite lists below
+
+            def on_update(self, delta_time):
+
+                pass
+
+        class Room:
+            #the space in wich the game takes place.  This class defines its area, properties, and other details.  Each room is a single level.
+            pass
+
+        class Camera:
+            #controlls what section of room is visible at any given time
+            pass
+
+def main():
+    game = Engine()
+    game.start()
+
+main()
+# %%
