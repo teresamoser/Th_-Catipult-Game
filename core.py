28a6698543ce01fddaf4e1b25d90d12920b8d7d8
jsappbyui1 <sap19001@byui.edu>
2021-10-12 23:50:30 -0600

    framework update

--- core.py
+++ core.py
@@ -2,14 +2,28 @@ import arcade
 import math
 
 
-class physics:
-    pass
+class engine:
+    # What runs the game.  This is for everything not visible to the player (mechanicly speeking)
+
+    class physics:
+        #the physics that runs the world and applys to all objects 
+
+        gravity = 9.8
+
+    class object():
+        #each item in the world that is not static is an object.  
+        pass
+
 
 class GUI:
     #The User interface.  This class contains all elements visible to the user.
-    
+
+    class window:
+        # the device that is used to view the game.  It is the box that appears on screen
+        pass
+
     class room:
-        #the space in wich the game takes place.  This class defines its area, properties, and other details.
+        #the space in wich the game takes place.  This class defines its area, properties, and other details.  Each room is a single level.
         pass
 
     class camera:
