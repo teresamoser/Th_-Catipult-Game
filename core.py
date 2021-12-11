a4d803451ecb8fec92c2727f9e6721f9eb4f3ab0
Teresa Moser <moserte@byui.edu>
2021-12-11 12:15:20 -0700

    New changes

--- core.py
+++ core.py
@@ -38,6 +38,22 @@ class Catapult_body(arcade.Sprite):
 
     def draw(self):
         pass
+    sg.theme('DarkAmber')   # Add a touch of color
+    #  All the stuff inside your window.
+    layout = [  [sg.Text('Determine Trajectory of Trebuchet')],
+                [sg.Text('Enter trajectory height: '), sg.InputText()],
+                [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+    # Create the Window
+    window = sg.Window('Trajectory Entry', layout)
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+             # if user closes window or clicks cancel
+            break
+        print('You entered ', values[0])
 
 
 def main():
@@ -51,7 +67,7 @@ def main():
             v = int(v)
         except:
             pass
-        game_settings[k] = v
+            game_settings[k] = v
 
     window = Game()
     window.setup()
